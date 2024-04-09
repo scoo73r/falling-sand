5cc6ea8633b7726ad139d5757939523c6c0d3b48
scoo73r <me@scooter.engineering>
2024-04-08 20:30:12 -0500

    it works! kinda with only one array

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/scoo73r/falling-sand
 
 go 1.22.2
 
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20240227114648-c3665eb9abf8
+
 require (
 	github.com/ebitengine/purego v0.7.1 // indirect
-	github.com/gen2brain/raylib-go/physics v0.0.0-20240227114648-c3665eb9abf8 // indirect
-	github.com/gen2brain/raylib-go/raylib v0.0.0-20240227114648-c3665eb9abf8 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,31 +1,68 @@
-package falling_sand
+package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"fmt"
 
-var sandMap [10][10]int
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
+const (
+	screenWidth = float32(1000)
+	screenHeight = float32(1000)
+)
+
+var (
+		sandMap [int(screenWidth)][int(screenHeight)]int
+		gameTick int
+
+)
 func drawMap() {
+		for x := 0; x < int(screenWidth); x++ {
+			for y := 0; y < int(screenHeight); y++ {
+			   if sandMap[x][y] == 1 {
+					rl.DrawCircle(int32(x) , int32(y), 30, rl.White)
+			}
+		}
+	}
+}
 
+func input() {
+if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+			sandMap[rl.GetMouseX()][rl.GetMouseY()] = 1
+		}
+}
+
+func gameLoop() {
+	for x := int(screenWidth)-1; x > 0; x--{
+		for y := int(screenHeight)-1; y > 0; y-- {
+		   if sandMap[x][y] == 0 && y < (int(screenHeight) - 1 ){
+				if sandMap[x][y-1] != 0{
+				sandMap[x][y] = sandMap[x][y-1] 
+                sandMap[x][y-1] = 0
+			}
+		}
+	}
+}
 }
 
 func main() {
-	screenWidth := float32(1000)
-	screenHeight := float32(500)
 
-	rl.SetConfigFlags(rl.FlagVsyncHint)
+
+	rl.SetTargetFPS(60)
 	rl.InitWindow(int32(screenWidth), int32(screenHeight), "Falling Sand")
-
+	gameTick = 0
 	for !rl.WindowShouldClose() {
 
-		if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
-			rl.DrawCircle(rl.GetMouseX(), 10, 10, rl.Green)
-			//	sandColumn := rl.GetMouseX()
-		}
+		gameTick++	
 		rl.BeginDrawing()
-
+		input()
+		//if gameTick == 10 {
+      	gameLoop()
+		//gameTick = 0}
+		drawMap()
 		rl.ClearBackground(rl.Black)
 		rl.DrawFPS(int32(screenWidth)-90, int32(screenHeight)-30)
-
+		rl.DrawText( fmt.Sprintf("%d", gameTick), int32(screenWidth)-90, int32(screenHeight)-60, 14, rl.White)
 		rl.EndDrawing()
 	}
 	rl.CloseWindow()
